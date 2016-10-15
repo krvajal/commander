93bf8da9fdf5daffcf8c5dbc451af0cf4053bb20
Miguel Carvajal <krvajal.miguelangel@gmail.com>
2016-10-15 18:53:28 -0300

    Add app.js

---
+++ app/scripts/app.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name sasystemCommanderApp
+ * @description
+ * # sasystemCommanderApp
+ *
+ * Main module of the application.
+ */
+angular.module('sasystemCommanderApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'firebase',
+    'firebase.ref',
+    'firebase.auth',
+    'ui.sortable',
+    'firebase.checkpointArray',
+    'ngMaterial'
+  ]);
